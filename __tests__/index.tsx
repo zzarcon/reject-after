5b50f35928b5ec5d4a7ddbb29047a1e569f757b0
Hector Leon Zarco Garcia <hezarco@gmail.com>
2019-05-08 23:36:16 +1000

    initial commit

---
+++ __tests__/index.tsx
@@ -0,0 +1,11 @@
+describe('rejectAfter', () => {
+  const setup = () => {
+    return {
+      
+    };
+  };
+  
+  it('Foo', () => {
+    expect(1).toEqual(1);
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export {rejectAfter} from './rejectAfter';
+export {rejectAfter as default} from './rejectAfter';
